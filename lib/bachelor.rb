91f33a1ae145ab7b8702fe7aacef5a32ed5781cf
Will Saffran <willh.saffran@gmail.com>
2019-02-13 00:38:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -25,7 +25,7 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.each do |season, array|
 end
 
 def get_occupation(data, hometown)
