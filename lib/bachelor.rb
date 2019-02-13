351c57409d70dee3583ef08d798c9d258c0f9679
Will Saffran <willh.saffran@gmail.com>
2019-02-13 00:40:13 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -15,8 +15,8 @@ end
 def get_contestant_name(data, occupation)
   data.each do |season, array|
     array.each do |people|
-      people.each do |k,v|
-        if v == occupation
+      people.each do |key,value|
+        if value == occupation
           return people["name"]
         end
       end
@@ -25,7 +25,17 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
+  count = 0
   data.each do |season, array|
+    array.each do |people|
+      people.each do |key, value|
+        if value == hometown
+          count += 1
+        end
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
