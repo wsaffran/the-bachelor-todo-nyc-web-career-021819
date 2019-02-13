8dbfe67d1657b220331fe093d2a79fd235d0044e
Will Saffran <willh.saffran@gmail.com>
2019-02-13 00:46:28 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -57,7 +57,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |people|
     people.each do |key, value|
       count += 1
-      total_age += people["age"]
+      total_age += people["age"].to_i
     end
   end
   average_age = total_age / count
